8fd71349c5b1bc2c99e1a8d1ee01b162fa523b01
shubhamj <shubham.cloudblitz@gmail.com>
2026-07-01 12:03:58 +0530

    new-pipeline

---
+++ jenkins-pipeline.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any 
+    stages {
+        stage ('PULL') {
+            steps {
+
+            }
+        }
+        stage ('BUILD') {
+            steps {
+
+            }
+        }
+        stage ('TEST') {
+            steps {
+
+            }
+        }
+        stage ('QUALITY-GATE') {
+            steps {
+
+            }
+        }
+        stage ('S3-UPLOAD') {
+            steps {
+                
+            }
+        }
+    }
+}
