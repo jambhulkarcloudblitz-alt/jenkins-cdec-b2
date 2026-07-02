cc19d04fa57b830d503226e3c6cd0d11faefd6e6
shubhamj <shubham.cloudblitz@gmail.com>
2026-07-02 12:25:06 +0530

    fix-pipeline

--- infra-pipeline.groovy
+++ infra-pipeline.groovy
@@ -3,7 +3,25 @@ pipeline {
     stages {
         stage('PULL') {
             steps {
-                
+                git branch: 'main', url: 'https://github.com/jambhulkarcloudblitz-alt/eks-infra-terraform.git'
+            }
+        }
+        stage ('PLAN') {
+            steps {
+                sh '''terraform init
+                    terraform plan '''
+           }
+        }
+        stage ('APPROVAL') {
+            steps {
+                timeout(30) {
+                            input 'Shall we proceed? ok: APPROVED'
+                            }
+            }
+        }
+        stage ('APPLY') {
+            steps {
+                sh 'terraform apply --auto-approve'
             }
         }
     }
